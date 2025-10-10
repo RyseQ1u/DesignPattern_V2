a0536615c4b4588cd40d52241b8c003144a2d2ec
RyseQ1u <460255780@qq.com>
2025-10-10 15:33:39 +0800

    Iterator bookShelf.books类型修改 遍历的代码不用调整

--- DesignPattern_V2/src/main/java/com/ryse/reationalmodel/_01_Iterator/BookShelf.java
+++ DesignPattern_V2/src/main/java/com/ryse/reationalmodel/_01_Iterator/BookShelf.java
@@ -1,25 +1,30 @@
 package com.ryse.reationalmodel._01_Iterator;
 
+import java.util.ArrayList;
+
 public class BookShelf implements  Aggregate{
-    private Book[] books;
+//    private Book[] books;
+    private ArrayList<Book> books;
     private int last;
 
     public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+//        this.books = new Book[maxsize];
+        this.books = new ArrayList<>(maxsize);
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+//        return books[index];
+        return books.get(index);
     }
 
     public boolean appendBook(Book book) {
-        if (last == books.length - 1) return false;
-        books[last++] = book;
+        books.add(book);
         return true;
     }
 
     public int getLength(){
-        return last;
+//        return last;
+        return books.size();
     }
 
     @Override
--- DesignPattern_V2/src/main/java/com/ryse/reationalmodel/_01_Iterator/IteratorMain.java
+++ DesignPattern_V2/src/main/java/com/ryse/reationalmodel/_01_Iterator/IteratorMain.java
@@ -7,6 +7,11 @@ public class IteratorMain {
         bookShelf.appendBook(new Book("Book2"));
         bookShelf.appendBook(new Book("Book3"));
         bookShelf.appendBook(new Book("Book4"));
+        //修改了集合类型
+        bookShelf.appendBook(new Book("Book5"));
+        bookShelf.appendBook(new Book("Book6"));
+        bookShelf.appendBook(new Book("Book7"));
+        bookShelf.appendBook(new Book("Book8"));
         Iterator iterator = bookShelf.iterator();
         while (iterator.hasNext()){
             System.out.println(((Book)iterator.next()).getName());
